f11863b581d6c7cb7ef9f1ccdf1fd90ee2a812e6
Storeyjm <storeyjm@dulwich.org.uk>
2021-09-10 12:14:40 +0100

    1.names

---
+++ #1.names.py
@@ -0,0 +1,10 @@
+#1.names
+
+#user inputs first name
+fname = input("Type in your first name")
+
+#user inputs surname
+sname = input("Type in your surname")
+
+#output full name
+print (fname,sname)
