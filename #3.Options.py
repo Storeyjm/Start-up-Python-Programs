3f644b5952a12bca8aabda10ced8e2cc37a03cb2
Storeyjm <storeyjm@dulwich.org.uk>
2021-09-12 23:05:47 +0100

    3.Options

---
+++ #3.Options.py
@@ -0,0 +1,12 @@
+#3.Options
+
+#User enters number
+Input = input("Enter a number between 1 & 3")
+
+#Converts input into integer
+num = int(Input)
+
+while num < 1 or num > 3:
+    Input = input("Enter a number between 1 & 3")
+    num = int(Input)
+print(num)
