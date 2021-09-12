6c86a6b1716de77dd844cfd81b30a88b0ceff1e9
Storeyjm <storeyjm@dulwich.org.uk>
2021-09-12 23:56:42 +0100

    5.Hundreds, tens and units

---
+++ #5.Hundreds, tens and units.py
@@ -0,0 +1,12 @@
+#5.Hundreds, tens and units
+
+#User inputs number
+num = int(input("Enter a number between 100 and 999"))
+
+#Splits numbers up
+a = num // 100
+b = (num - (a * 100)) // 10
+c = num - (a * 100) - (b * 10)
+
+#Prints output
+print(a, "hundreds", b, "tens", c, "units.")
