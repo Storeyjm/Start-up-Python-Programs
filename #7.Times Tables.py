c077058068b0daf1b79760b2546548e489957db0
Storeyjm <storeyjm@dulwich.org.uk>
2021-09-13 09:34:20 +0100

    7.Times Tables

---
+++ #7.Times Tables.py
@@ -0,0 +1,20 @@
+#7.Times Tables
+
+#User inputs number
+Num = int(input("Pick a number between 1 and 10"))
+
+#Times tables
+one = Num
+two = (Num * 2)
+three = (Num * 3)
+four = (Num * 4)
+five = (Num * 5)
+six = (Num * 6)
+seven = (Num * 7)
+eight = (Num * 8)
+nine = (Num * 9)
+ten = (Num * 10)
+
+#Prints output
+print(one, two, three, four, five, six, seven, eight, nine, ten)
+#End print
