324272aeebae28b5452ddd0e2837e61591071296
Storeyjm <storeyjm@dulwich.org.uk>
2021-09-13 09:34:31 +0100

    8.Comparison of Two

---
+++ #8.Comparison of Two.py
@@ -0,0 +1,12 @@
+#8.Comparison of Two
+
+#User Inputs numbers
+Num1 = int(input("Enter a number"))
+Num2 = int(input("Enter a different number"))
+
+#If statement and print
+if Num1 > Num2:
+    print(Num1, Num2)
+else:
+        print(Num2, Num1)
+#end if
