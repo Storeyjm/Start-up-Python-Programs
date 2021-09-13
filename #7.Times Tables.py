977f3068dde97086121cdf99f6e6e353d50e00b6
Storeyjm <storeyjm@dulwich.org.uk>
2021-09-13 12:07:39 +0100

    7.Times Tables

--- #7.Times Tables.py
+++ #7.Times Tables.py
@@ -1,20 +1,13 @@
 #7.Times Tables
 
 #User inputs number
-Num = int(input("Pick a number between 1 and 10"))
+iNum = int(input("Pick a number between 1 and 10"))
+
+#counter initiate
+counter = 1
 
 #Times tables
-one = Num
-two = (Num * 2)
-three = (Num * 3)
-four = (Num * 4)
-five = (Num * 5)
-six = (Num * 6)
-seven = (Num * 7)
-eight = (Num * 8)
-nine = (Num * 9)
-ten = (Num * 10)
-
-#Prints output
-print(one, two, three, four, five, six, seven, eight, nine, ten)
-#End print
+while counter < 11:
+    print(iNum * counter)
+    counter = counter + 1
+#end while
