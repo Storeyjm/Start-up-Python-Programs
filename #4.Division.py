e8b53e0dc0612f292bdb384c22159c25ec253721
Storeyjm <storeyjm@dulwich.org.uk>
2021-09-14 18:23:25 +0100

    Division

--- #4.Division.py
+++ #4.Division.py
@@ -1,12 +1,8 @@
 #4.Division
 
 #User inputs number
-Input1 = input("Enter an integer")
-Input2 = input("Enter a divisor")
-
-#Converts into a float
-Integer = float(Input1)
-Divisor = float(Input2)
+Integer = int(input("Enter an integer"))
+Divisor = int(input("Enter a divisor"))
 
 #Divides integer by dividor
 a = (Integer // Divisor)
@@ -14,7 +10,3 @@ b = (Integer % Divisor)
 
 #Prints output
 print (a,"remainder", b)
-
-## Your print statemeent needs commas .. In have put them in
-## Also I don't think there is any need to covert to float as the results are integers. 
-## Convert inputs to int I would think
