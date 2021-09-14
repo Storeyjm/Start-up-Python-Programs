92228eb0437e0b5910e267c20b8d26bec7b46db7
Storeyjm <storeyjm@dulwich.org.uk>
2021-09-14 18:23:40 +0100

    Hundreds, tens and units

--- #5.Hundreds, tens and units.py
+++ #5.Hundreds, tens and units.py
@@ -3,12 +3,12 @@
 #User inputs number
 num = int(input("Enter a number between 100 and 999"))
 
-#Splits numbers up
+#Hundreds
 a = num // 100
+#Tens
 b = (num - (a * 100)) // 10
+#Units
 c = num - (a * 100) - (b * 10)
 
 #Prints output
 print(a, "hundreds", b, "tens", c, "units.")
-
-## ACS - Excellent work.  Maybe a few more comments.
