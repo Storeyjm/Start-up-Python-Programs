dff37c55f14d8b068642b6bfc1733ae220c97175
dracstorey <dracstorey@gmail.com>
2021-09-19 21:44:51 +0100

    Task 10 - ACS help

--- #10.Words Words Words.py
+++ #10.Words Words Words.py
@@ -2,22 +2,35 @@
 
 #User inputs sentence
 myString = input("Enter a sentence")
+
 #initiate words counter
 words = 0
-#initiate space.flag
-space.flag = True
+
+#initiate space_flag
+space_flag = True
+
+
 #Reads number of words in sentence using loop
 #initiate counter
 counter = 0
-while counter<len(myString):
-    while space.flag = True:
-        if myString = " ":
-            space.flag = False
-    if space.flag = False:
-        words = words + 1
-        #count until myString /= " "
-        
+while counter < len(myString):
+    
+    # check if character is a space
+    if myString[counter] == " ":
+        # set space_flag to true and ignore character
+        space_flag = True
+    else:
+        # if space_flag is true then new word so add 1
+        if space_flag == True:
+            words = words + 1
+        # set space_flag to False as it is not a spce
+        space_flag = False
+        # endif
+    #endif
+    
+    #increment counter'
     counter = counter + 1
 #endwhile
 
 #Prints final value of words
+print (words)
